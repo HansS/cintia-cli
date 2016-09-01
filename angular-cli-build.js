41247fc0b103b65a6161c97cdabe9a5f8feef514
hansschenker <gian.schenker@gmail.com>
2016-09-01 20:26:17 +0200

    build

---
+++ angular-cli-build.js
@@ -0,0 +1,9 @@
+vendorNpmFiles: [
+      'systemjs/dist/system-polyfills.js',
+      'systemjs/dist/system.src.js',
+      'zone.js/dist/*.js',
+      'es6-shim/es6-shim.js',
+      'reflect-metadata/*.js',
+      'rxjs/**/*.js',
+      '@angular/**/*.js'
+    ]
